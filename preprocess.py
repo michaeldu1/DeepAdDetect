22b58086ac4224ccab58cdfba018205c3109e4f1
Michael Du <mdu7@stanford.edu>
2020-05-11 00:31:21 -0500

    fixed preprocess

--- preprocess.py
+++ preprocess.py
@@ -21,8 +21,12 @@ def find_min_dimensions():
 
 def resize_images(width, height):
   os.mkdir('./data-resized')
-  for folder in list(range(11)) + ["resnet_training_negatives"]:
-    curr_path = "./data/" + str(folder) + "/*.jpg"
+  for folder in list(range(11)):
+    curr_path = "./data/" + str(folder)
+    if folder == 10:
+      curr_path += "/*.png"
+    else:
+      curr_path += "/*.jpg"
     new_dest = "./data-resized/" + str(folder) + "-resized/"
     os.mkdir(new_dest)
     print(curr_path)
@@ -30,6 +34,16 @@ def resize_images(width, height):
       im = Image.open(img_path)
       new_img = im.resize((width, height))
       new_img.save(new_dest + img_path[9:], im.format)
+  
+  os.mkdir("./data-resized/resnet_training_negatives_resized/")
+  for idx in range(5):
+    curr_path = "./data/" + "resnet_training_negatives/set"
+    dest = './data-resized/resnet_training_negatives_resized/set' + str(idx)
+    os.mkdir(dest)
+    for img_path in glob.glob(curr_path + str(idx) + "/*.png"):
+      im = Image.open(img_path)
+      new_img = im.resize((width, height))
+      new_img.save(dest + img_path[38:], im.format)
 
 # min_width, min_height = find_min_dimensions()
 resize_images(256, 256)
